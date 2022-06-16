c2eb5f9f7911324bdda323a01f19109186d85c79
Manuel Lao <manolinlao@gmail.com>
2022-06-16 21:11:21 +0200

    mostrar pares de peliculas

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SlideshowBackdropComponent} from './slideshow-backdrop/slideshow-backdrop.component';
+import {IonicModule} from '@ionic/angular';
+import {PipesModule} from '../pipes/pipes.module';
+import {SlideshowPosterComponent} from './slideshow-poster/slideshow-poster.component';
+import {SlideshowParesComponent} from './slideshow-pares/slideshow-pares.component';
+
+@NgModule({
+    declarations: [SlideshowBackdropComponent, SlideshowPosterComponent, SlideshowParesComponent],
+    imports: [CommonModule, IonicModule, PipesModule],
+    exports: [SlideshowBackdropComponent, SlideshowPosterComponent, SlideshowParesComponent],
+})
+export class ComponentsModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesService } from '../services/movies.service';
-import { Pelicula } from '../interfaces/interfaces';
+import {Component, OnInit} from '@angular/core';
+import {MoviesService} from '../services/movies.service';
+import {Pelicula} from '../interfaces/interfaces';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-  peliculasRecientes: Pelicula[] = [];
+    peliculasRecientes: Pelicula[] = [];
+    peliculasPopulares: Pelicula[] = [];
 
-  slideOpts = {
-    slidesPerView: 1.1,
-    freeMode: true,
-  };
+    constructor(private moviesService: MoviesService) {}
+    ngOnInit(): void {
+        this.moviesService.getFeature().subscribe((resp) => {
+            console.log(resp.results);
+            this.peliculasRecientes = resp.results;
+        });
 
-  constructor(private moviesService: MoviesService) {}
-  ngOnInit(): void {
-    this.moviesService.getFeature().subscribe((resp) => {
-      console.log(resp.results);
-      this.peliculasRecientes = resp.results;
-    });
-  }
+        this.moviesService.getPopulares().subscribe((resp) => {
+            this.peliculasPopulares = resp.results;
+        });
+    }
 }
